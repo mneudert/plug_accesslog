b38cd87d8e136cf894ae46b6de37494a7e3a5449
Marc Neudert <marc.neudert@gmail.com>
2015-05-11 21:36:57 +0200

    uses only lowercase to access response headers

--- test/plug/accesslog/formatter/response_header_test.exs
+++ test/plug/accesslog/formatter/response_header_test.exs
@@ -7,7 +7,9 @@ defmodule Plug.AccessLog.Formatter.ResponseHeaderTest do
   test "%{VARNAME}o" do
     header = "X-Test-Header"
     value  = "test value for %o"
-    conn   = conn(:get, "/") |> put_resp_header(header, value)
+    conn   =
+         conn(:get, "/")
+      |> put_resp_header(header |> String.downcase, value)
 
     assert value == Formatter.format("%{#{ header }}o", conn)
     assert "-"   == Formatter.format("%{X-Unknown-Header}o", conn)
