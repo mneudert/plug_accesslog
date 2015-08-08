5f97af7fee8cf0d231d7c9a4f1306179b43c644f
Marc Neudert <marc.neudert@gmail.com>
2015-08-08 15:29:23 +0200

    bumps version to v0.9.0-dev

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule Plug.AccessLog.Mixfile do
       name:          "Plug.AccessLog",
       description:   "Plug for writing access logs",
       package:       package,
-      version:       "0.8.0",
+      version:       "0.9.0-dev",
       elixir:        "~> 1.0",
       deps:          deps(Mix.env),
       docs:          docs,
@@ -43,7 +43,7 @@ defmodule Plug.AccessLog.Mixfile do
   def docs do
     [ main:       "readme",
       readme:     "README.md",
-      source_ref: "v0.8.0",
+      source_ref: "master",
       source_url: @url_github ]
   end
 
