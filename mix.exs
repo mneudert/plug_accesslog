12b32a4d657859c59cdf0fd10d6e8bffac61267e
Marc Neudert <marc.neudert@gmail.com>
2016-01-26 20:40:44 +0100

    modernizes mix file

--- mix.exs
+++ mix.exs
@@ -4,15 +4,19 @@ defmodule Plug.AccessLog.Mixfile do
   @url_github "https://github.com/mneudert/plug_accesslog"
 
   def project do
-    [ app:           :plug_accesslog,
-      name:          "Plug.AccessLog",
+    [ app:     :plug_accesslog,
+      name:    "Plug.AccessLog",
+      version: "0.11.0-dev",
+      elixir:  "~> 1.0",
+      deps:    deps,
+
+      build_embedded:  Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+
       description:   "Plug for writing access logs",
-      package:       package,
-      version:       "0.11.0-dev",
-      elixir:        "~> 1.0",
-      deps:          deps(Mix.env),
       docs:          docs,
-      test_coverage: [ tool: ExCoveralls ]]
+      package:       package,
+      test_coverage: [ tool: ExCoveralls ] ]
   end
 
   def application do
@@ -20,37 +24,31 @@ defmodule Plug.AccessLog.Mixfile do
       mod:          { Plug.AccessLog.Application, [] } ]
   end
 
-  def deps(:docs) do
-    deps(:prod) ++
-      [ { :earmark, "~> 0.2",  optional: true },
-        { :ex_doc,  "~> 0.11", optional: true } ]
-  end
+  defp deps do
+    [ { :earmark, "~> 0.2",  only: :docs },
+      { :ex_doc,  "~> 0.11", only: :docs },
 
-  def deps(:test) do
-    deps(:prod) ++
-      [ { :dialyze,     "~> 0.2", optional: true },
-        { :excoveralls, "~> 0.4", optional: true } ]
-  end
+      { :dialyze,     "~> 0.2", only: :test },
+      { :excoveralls, "~> 0.4", only: :test },
 
-  def deps(_) do
-    [ { :timex,  "~> 1.0" },
+      { :timex,  "~> 1.0" },
       { :tzdata, "~> 0.5" },
 
       { :cowboy, "~> 1.0", optional: true },
       { :plug,   "~> 1.0", optional: true } ]
   end
 
-  def docs do
+  defp docs do
     [ extras:     [ "CHANGELOG.md", "README.md" ],
       main:       "readme",
       source_ref: "master",
       source_url: @url_github ]
   end
 
-  def package do
+  defp package do
     %{ files:       [ "CHANGELOG.md", "LICENSE", "mix.exs", "README.md", "lib" ],
        licenses:    [ "Apache 2.0" ],
        links:       %{ "GitHub" => @url_github },
-       maintainers: [ "Marc Neudert" ]}
+       maintainers: [ "Marc Neudert" ] }
   end
 end
