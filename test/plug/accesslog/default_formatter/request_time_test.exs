c487865faf79cc5d2fd84306b26f9a0288756d62
Marc Neudert <marc.neudert@gmail.com>
2016-03-25 19:48:49 +0100

    uses Time.DateTime for request time formatter test

--- test/plug/accesslog/default_formatter/request_time_test.exs
+++ test/plug/accesslog/default_formatter/request_time_test.exs
@@ -6,16 +6,12 @@ defmodule Plug.AccessLog.DefaultFormatter.RequestTimeTest do
   alias Plug.AccessLog.DefaultFormatter
 
   test "%t" do
-    datetime  = {{ 2015, 01, 10 }, { 14, 46, 18 }}
-    conn      =
+    timezone = Timex.timezone("America/Chicago", { 2015, 01, 10 })
+    datetime = Timex.datetime({{ 2015, 01, 10 }, { 14, 46, 18 }}, timezone)
+    conn     =
          conn(:get, "/")
       |> put_private(:plug_accesslog, %{ local_time: datetime })
 
-    formatted =
-         datetime
-      |> DateTime.from()
-      |> Timex.format!("[%d/%b/%Y:%H:%M:%S %z]", :strftime)
-
-    assert formatted == DefaultFormatter.format("%t", conn)
+    assert "[10/Jan/2015:14:46:18 -0600]" == DefaultFormatter.format("%t", conn)
   end
 end
