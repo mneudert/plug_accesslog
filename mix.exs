1be2fa1701f83a7192bdad263e8950e1777eb8a5
Marc Neudert <marc.neudert@gmail.com>
2016-06-25 15:45:49 +0200

    bumps version to v0.13.0-dev

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Plug.AccessLog.Mixfile do
   def project do
     [ app:     :plug_accesslog,
       name:    "Plug.AccessLog",
-      version: "0.12.0",
+      version: "0.13.0-dev",
       elixir:  "~> 1.2",
       deps:    deps(),
 
@@ -49,7 +49,7 @@ defmodule Plug.AccessLog.Mixfile do
   defp docs do
     [ extras:     [ "CHANGELOG.md", "README.md" ],
       main:       "readme",
-      source_ref: "v0.12.0",
+      source_ref: "master",
       source_url: @url_github ]
   end
 
