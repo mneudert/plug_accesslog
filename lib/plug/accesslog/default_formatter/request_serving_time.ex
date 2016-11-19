d0049a97a52e4a945dff7a9e52ab3a0441f8819a
Marc Neudert <marc.neudert@gmail.com>
2016-11-19 17:45:47 +0100

    ws / cs

--- lib/plug/accesslog/default_formatter/request_serving_time.ex
+++ lib/plug/accesslog/default_formatter/request_serving_time.ex
@@ -1,14 +1,17 @@
 defmodule Plug.AccessLog.DefaultFormatter.RequestServingTime do
   @moduledoc """
-  Calculates the seconds (or milliseconds) taken to serve the request.
+  Calculates the time taken to serve the request.
   """
 
   use Timex
 
+  @type time_format :: String.t | :seconds | :milliseconds | :microseconds
+
+
   @doc """
   Formats the log output.
   """
-  @spec format(Plug.Conn.t, String.t | :seconds | :milliseconds | :microseconds) :: iodata
+  @spec format(Plug.Conn.t, time_format) :: iodata
   def format(conn, "s"),  do: format(conn, :seconds)
   def format(conn, "ms"), do: format(conn, :milliseconds)
   def format(conn, "us"), do: format(conn, :microseconds)
