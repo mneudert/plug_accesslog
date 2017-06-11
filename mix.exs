5cee4f80557334acdb3cc882ed704780291fd649
Marc Neudert <marc.neudert@gmail.com>
2017-06-11 16:06:05 +0200

    updates dependencies (and their minimum versions)

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule Plug.AccessLog.Mixfile do
 
   defp deps do
     [ { :ex_doc,      ">= 0.0.0", only: :dev },
-      { :excoveralls, "~> 0.6",   only: :test },
+      { :excoveralls, "~> 0.7",   only: :test },
 
       { :timex,  "~> 3.0" },
 
