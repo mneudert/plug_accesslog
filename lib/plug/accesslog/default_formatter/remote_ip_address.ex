ad6f65f7d2fb353ad02c9f8f02846b6418ccbe27
Marc Neudert <marc.neudert@gmail.com>
2020-06-10 19:34:02 +0200

    Replace :inet_parse.ntoa/1 with :inet.ntoa/1

--- lib/plug/accesslog/default_formatter/remote_ip_address.ex
+++ lib/plug/accesslog/default_formatter/remote_ip_address.ex
@@ -7,5 +7,5 @@ defmodule Plug.AccessLog.DefaultFormatter.RemoteIPAddress do
   Formats the log output.
   """
   @spec format(Plug.Conn.t()) :: iodata
-  def format(%{remote_ip: remote_ip}), do: :inet_parse.ntoa(remote_ip)
+  def format(%{remote_ip: remote_ip}), do: :inet.ntoa(remote_ip)
 end
